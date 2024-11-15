7390deea28c7a2c11ae2a8848b6fb240bb59854a
adityasunny1189 <adityapathak1189@gmail.com>
2024-11-15 08:51:29 +0530

    fixed comments

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,11 +6,11 @@ const PROD_DB_PATH = process.env.PROD_DB_PATH;
 const ENV = process.env.ENV;
 console.log("Env: ", ENV);
 
-let DB_PATH = "sqlite::memory:";
+let DB_PATH = PROD_DB_PATH;
 
-if (ENV === "production") {
-    DB_PATH = PROD_DB_PATH;
-    console.log("Connecting to production DB");
+if (ENV === "development") {
+    DB_PATH = "sqlite::memory:";
+    console.log("Connecting to development DB");
 }
 
 export const sequelize = new Sequelize(DB_PATH);
