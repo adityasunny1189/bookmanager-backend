3e42d069dd304c603a45960a7e5b892fd8715804
adityasunny1189 <adityapathak1189@gmail.com>
2024-11-15 08:26:33 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-import express, { json, urlencoded } from 'express';
-import cors from 'cors';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs } from './schema/typeDefs.js';
-import { resolvers } from './resolvers/resolvers.js';
-import { authorLoader } from './loaders/authorLoader.js';
-import Book from './models/Book.js';
-import Author from './models/Author.js';
-import associateModels from './models/models.js';
-import { sequelize } from './utils/database.js';
-import { bookLoader } from './loaders/bookLoader.js';
-import { connect } from 'mongoose';
-import { reviewLoader } from './loaders/reviewLoader.js';
-import { config } from 'dotenv';
-config({ path: './src/config.env' });
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs } from "./schema/typeDefs.js";
+import { resolvers } from "./resolvers/resolvers.js";
+import { authorLoader } from "./loaders/authorLoader.js";
+import Book from "./models/Book.js";
+import Author from "./models/Author.js";
+import associateModels from "./models/models.js";
+import { sequelize } from "./utils/database.js";
+import { bookLoader } from "./loaders/bookLoader.js";
+import { connect } from "mongoose";
+import { reviewLoader } from "./loaders/reviewLoader.js";
+import { config } from "dotenv";
+config({ path: "./src/config.env" });
 
 // Create models association
-const models = {Book, Author};
+const models = { Book, Author };
 associateModels(models);
 
 // Initialize sequelize (connect to the database)
@@ -35,53 +35,60 @@ const MONGO_DB_PATH = process.env.MONGO_DB_PATH;
 
 connect(MONGO_DB_PATH, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((data) => {
-    console.log("connected to mongo db");
-}).catch(err => {
-    console.log("Error connecting to mongo DB: ", err);
-    process.exit(1);
-});
+    useUnifiedTopology: true,
+})
+    .then((data) => {
+        console.log("connected to mongo db");
+    })
+    .catch((err) => {
+        console.log("Error connecting to mongo DB: ", err);
+        process.exit(1);
+    });
 
 // Create an express app
 export const app = express();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 // Create an Apollo Graphql Server
 const graphqlServer = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
 });
 
 await graphqlServer.start();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json({
-        message: "Hello World"
+        message: "Hello World",
     });
 });
 
-app.use("/graphql", expressMiddleware(graphqlServer, {
-    context: ({ req }) => {
-        return {
-            loaders: {
-                authorLoader: authorLoader,
-                bookLoader: bookLoader,
-                reviewLoader: reviewLoader
-            },
-            name: "Bookmanager Context"
-        }
-    }
-}));
+app.use(
+    "/graphql",
+    expressMiddleware(graphqlServer, {
+        context: ({ req }) => {
+            return {
+                loaders: {
+                    authorLoader: authorLoader,
+                    bookLoader: bookLoader,
+                    reviewLoader: reviewLoader,
+                },
+                name: "Bookmanager Context",
+            };
+        },
+    })
+);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
     res.status(404).json({
         status: `fail`,
-        message: `can't find the ${req.method} on url ${req.originalUrl} on the server`
+        message: `can't find the ${req.method} on url ${req.originalUrl} on the server`,
     });
 });
